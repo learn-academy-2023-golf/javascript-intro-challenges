5c181143ae68d1153a2f072751008b3bca6f7b10
Jerrod <chaddjed@gmail.com>
2023-10-04 15:55:15 -0700

    JS-Intro-challenges add

---
+++ conditionals-jerrod-corey-chris.js
@@ -0,0 +1,28 @@
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true
+
+
+let num1 = 34/3
+let num2 = 67/2
+if (num1>num2){
+    console.log(`${num1} Incorrect ${num2}`)
+} else {
+    console.log(`${num2} correct ${num1}`)
+}
+console.log(5 == "5")
+console.log(5 === "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 && "Student".length === 7)
+console.log("LEARN".length === 5 || "Student".length === 10)
+console.log("LEARN".includes("RN"))
+console.log("LEARN".includes("rn"))
+console.log("LEARN"[0] === "l")
+console.log("LEARN"[0] === "L")
