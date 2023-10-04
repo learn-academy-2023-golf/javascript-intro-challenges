3d1a91a97c992e79e38c918aeb45ea14e0e092d6
Derek Maly <dmaly18@gmail.com>
2023-10-04 12:10:36 -0700

    problem 1

--- javascript-intro-derek-markayel.js
+++ javascript-intro-derek-markayel.js
@@ -1,5 +1,5 @@
 // Write the code that will log the output of 34 added to 71.
-
+console.log(71+34)
 // Write the code that will log the output of 67 subtracted from 123.
 // Write the code that will log the output of 56 multiplied by 23.
 // Write the code that will log the output of 45 divided by 5.
