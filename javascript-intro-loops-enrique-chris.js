0fae6afef0b961cc40751dcf8308773a1146e04d
Rique <aguirree1985@gmail.com>
2023-10-05 15:49:46 -0700

    Start of loop challenge

---
+++ javascript-intro-loops-enrique-chris.js
@@ -0,0 +1,89 @@
+// Create a for loop that logs each number from 1 - 20.
+// for( let count=1;count <=20; count++){
+//     console.log(count)
+// }
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+// 20
+// Create a for loop that logs every other number from 1 - 20.
+// for( let count=0;count <=20; count = count +2){
+//     console.log(count)
+// }
+// 0
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+// 20
+
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// for( let count=0;count <= 20; count++){
+//     console.log(count * 3)
+// }
+// 0
+// 3
+// 6
+// 9
+// 12
+// 15
+// 18
+// 21
+// 24
+// 27
+// 30
+// 33
+// 36
+// 39
+// 42
+// 45
+// 48
+// 51
+// 54
+// 57
+// 60
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// for( let count = 1;count <= 20; count++){
+//    if(count % 2 === 0){
+//     console.log(count)
+//    } else if (count % 2 === 1){
+//     console.log("ODD")
+//    }
+// }
+
+
+// Consider this variable:
+
+// const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+// Create the code that will log the smallest number from the array.
+// Create the code that will log the remainder of each number when divided by 2.
+// Expected output: 1, 1, -1, 0, 1
+// Consider this variable:
+
+// const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+// Create the code that will log every other character in the string.
