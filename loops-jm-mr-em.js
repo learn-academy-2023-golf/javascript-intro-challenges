17024deb1cea237c6ae4b4d2be2368653a1fa626
Eric McKee <lyolayale19@gmail.com>
2023-10-05 15:29:28 -0700

    Updated loop challenge.

---
+++ loops-jm-mr-em.js
@@ -0,0 +1,53 @@
+// 💻 Challenges
+// 1. Create a for loop that logs each number from 1 - 20.
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+// 2. Create a for loop that logs every other number from 1 - 20.
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// 3. Create a for loop that logs the result of each number from 1 - 20 tripled.
+
+// for (let i = 0; i <= 20; i += 3) {
+//     console.log(i);
+// }
+
+// 4. Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// Consider this variable:
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 !== 0) {
+//         console.log("ODD");
+//     } else {
+//         console.log(i);
+//     }
+   
+// }
+
+
+
+// const nums = [3, 57, -9, 20, 67]
+// 5. Create the code that will log the largest number from the array.
+
+// let max = 0;
+// for (let i = 0; i < nums.length; i++) {
+
+//     if (nums[i] > max) max = nums[i];
+// }
+// console.log(max);
+
+
+// Create the code that will log the smallest number from the array.
+// Create the code that will log the remainder of each number when divided by 2.
+// Expected output: 1, 1, -1, 0, 1
+// Consider this variable:
+
+// const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+// Create the code that will log every other character in the string.
